5a02d71558e4d1e888ec28842410bb3b71dd99e1
Artjom Becker <artjomb.2001@gmail.com>
2026-03-30 16:27:25 +0200

    feat: add contact form backend

--- .wrangler/tmp/bundle-MsvrEe/middleware-insertion-facade.js
+++ .wrangler/tmp/bundle-MsvrEe/middleware-insertion-facade.js
@@ -1,8 +1,8 @@
-				import worker, * as OTHER_EXPORTS from "/Users/artjombecker/Documents/Bildung/Projekte/portfolio-1/.wrangler/tmp/pages-4RcnYn/functionsWorker-0.9750359076789057.mjs";
+				import worker, * as OTHER_EXPORTS from "/Users/artjombecker/Documents/Bildung/Projekte/portfolio-1/.wrangler/tmp/pages-0URyE7/functionsWorker-0.7489085158909257.mjs";
 				import * as __MIDDLEWARE_0__ from "/Users/artjombecker/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
 import * as __MIDDLEWARE_1__ from "/Users/artjombecker/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
 
-				export * from "/Users/artjombecker/Documents/Bildung/Projekte/portfolio-1/.wrangler/tmp/pages-4RcnYn/functionsWorker-0.9750359076789057.mjs";
+				export * from "/Users/artjombecker/Documents/Bildung/Projekte/portfolio-1/.wrangler/tmp/pages-0URyE7/functionsWorker-0.7489085158909257.mjs";
 				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
